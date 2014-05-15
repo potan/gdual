bf67f7d32e6b8bfbdf4352571e684c3803ff6025
Mike Potanin <potanin@gmail.com>
2014-05-15 11:29:08 +0400

    No ignore near zero values.

--- Math/GDual/Demo/SimpleSparseSolver.hs
+++ Math/GDual/Demo/SimpleSparseSolver.hs
@@ -2,13 +2,13 @@ module Math.GDual.Demo.SimpleSparseSolver (solve) where
 
 import Linear.Epsilon
 
-zm c = z where
+zm n c = z where
   z [] a = a
   z a [] = map (\(n,v) -> (n,c*v)) a
   z (xl@((nx,x):xs)) (yl@((ny,y):ys)) | nx == ny = (let r = (z xs ys)
                                                         v = y+c*x
                                                     in
-                                                        if nearZero v
+                                                        if (n == nx) || (v == 0)
                                                         then r
                                                         else (nx, v):r)
                                       | nx < ny = (nx,c*x):(z xs yl)
@@ -18,7 +18,7 @@ kill1 _ [] l = l
 kill1 (n,v) x y =
     case filter ((n ==) . fst) y of
      [] -> y
-     ((_,vy):_) -> zm (negate (vy/v)) x y
+     ((_,vy):_) -> zm n (negate (vy/v)) x y
 
 diag l [] = reverse l
 diag l (x:xs) = let
